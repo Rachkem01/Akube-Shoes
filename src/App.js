5d8de6713f2c1e6feee2a35e607e0621897516b3
Rachael <agunlokokemi01@gmail.com>
2024-06-25 22:14:02 +0100

    json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState} from "react"
+import './index.css'
+import Navigation from "./Navigation/Nav"
+import Products from "./Products/Products"
+import Recommended from "./Recommended/Recommended"
+import Sidebar from "./Sidebar/Sidebar"
+import './App.css'
+import products from "./db/data"
+import Card from "./components/Card"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const[selectedCategory, setSelectedCategory] =useState(null)
+
+// input filter
+const[query, setQuery] =useState("")
+
+const handleInputChange = (event) =>{
+    setQuery(event.target.value)
+}
+
+const filteredItems = products.filter((product)=>product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1);
+// input filter
+
+// Radio filter
+const handleChange = (event) => {
+    setSelectedCategory(event.target.value)
+}
+// Button Filter
+const handleClick = (event) =>{
+    setSelectedCategory(event.target.value)
+}
+
+function filteredData(products, selected,query){
+let filteredProducts = products
+
+// Filtering Input Items
+if(query){
+   filteredProducts = filteredItems
+}
+// Selected Filter
+if(selected){
+    filteredProducts = filteredProducts.filter(({category,color,company,title,newPrice})=> category === selected || color === selected || company === selected || newPrice === selected || title === selected)
+}
+return filteredProducts.map(
+    ({ img, title, star, reviews, prevPrice, newPrice }) => (
+      <Card
+        key={Math.random()}
+        img={img}
+        title={title}
+        star={star}
+        reviews={reviews}
+        prevPrice={prevPrice}
+        newPrice={newPrice}
+      />
+    )
   );
 }
 
-export default App;
+ const result = filteredData(products,selectedCategory, query)
+
+
+
+return <>
+
+    <Sidebar handleChange ={handleChange} />
+<Navigation query ={query} handleInputChange={handleInputChange} />
+<Recommended handleClick={handleClick} />
+<Products  result= {result}/>
+
+    </>
+}
+export default App
